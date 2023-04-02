58c1c519818430442e4829b7a3d3559e6ce4c3a1
Gao Rui <125622774+gaoray08@users.noreply.github.com>
2023-04-02 18:33:23 +0800

    Updated with Amos' help

--- TestCalculator.cpp
+++ TestCalculator.cpp
@@ -8,62 +8,103 @@ int main()
 {
     int numberOfNumbers;
     int count = 1;
-    int sum = 0;
+    float sum = 0;
     string operators;
     string x;
-    string myList[numberOfNumbers] = { };
+    vector<string> myList;
     
     cout << "Welcome to a command line calculator, made by Gao Rui. \n ";
     cout << "Please enter the number of arguements you are going to key in. \n";
     cin >> numberOfNumbers;
-    cout << "Please enter the operator that you want to use (+ or - or * or /)";
+    cout << "Please enter the operator that you want to use (+ or - or * or /) \n";
     cin >> operators; 
 
-    while (count < numberOfNumbers)
+    while (count <= numberOfNumbers)
     {
-        cout << "Please enter the " << count << " number.";
+        cout << "Please enter the " << count << " number. \n";
         cin >> x;
         myList.push_back(x);
-        count =+ 1
-
+        count += 1;
+ 
     }
 
     if (operators == "+")
     {
-        for (int i : myList){
-            sum += i;
+        for (int i=0;i < myList.size();i++){
+            sum += stoi(myList[i]);
         }
-        cout << "The sum of " << myList << " is " << endl;
+        cout << "The sum of ";
+        for (int i=0;i<myList.size();i++){
+          cout << myList[i];
+          if (i < myList.size()-1){
+            cout << " and ";
+          }
+        }
+        cout << " is ";
         cout << sum << endl;
     }
-
-    if (operators == "-")
+    else if (operators == "-")
     {
-        for (int i : myList){
-            sum -= i;
+        for (int i=0;i < myList.size();i++){
+            if (i==0){
+              sum = stoi(myList.at(i));
+            }
+          else{
+            sum -= stoi(myList.at(i));
+          }
         }
-        cout << "The sum of " << myList << " is " << endl;
+        cout << "The minus of ";
+        for (int i=0;i<myList.size();i++){
+          if(i==0){
+            cout << myList[i];
+          }
+          if (i!=0){
+            cout << " minus ";
+            cout << myList[i];
+          }
+        }
+        cout << " is ";
         cout << sum << endl;
     }
-
-    if (operators == "*")
-    {
-        for (int i : myList){
-            sum *= i;
-        }
-        cout << "The sum of " << myList << " is " << endl;
-        cout << sum << endl;
+  else if (operators == "*"){
+    for (int i=0;i < myList.size();i++){
+      if (i==0){
+        sum = 1;
+      }
+      sum *= stoi(myList.at(i));
     }
-
-    if (operators == "/")
-    {
-        for (int i : myList){
-            sum /= i;
+    cout << "The multiplcation of ";
+        for (int i=0;i<myList.size();i++){
+          cout << myList[i];
+          if (i < myList.size()-1){
+            cout << " and ";
+          }
         }
-        cout << "The sum of " << myList << " is " << endl;
+        cout << " is ";
         cout << sum << endl;
-    }
-
-    cout << "Thank you for using my calculator!"
+  }
+  else if (operators == "/"){
+    for (int i=0;i < myList.size();i++){
+            if (i==0){
+              sum = stoi(myList.at(i));
+            }
+          else{
+            sum /= stoi(myList.at(i));
+          }
+        }
+        cout << "The divison of ";
+        for (int i=0;i<myList.size();i++){
+          if(i==0){
+            cout << myList[i];
+          }
+          if (i!=0){
+            cout << " divided by ";
+            cout << myList[i];
+          }
+        }
+        cout << " is ";
+        cout << sum << endl;
+  }
+    cout << "Thank you for using my calculator!";
     return 0; 
 }
